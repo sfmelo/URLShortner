756bc8c6359036644c99e06e5fa18cc2fb8bdac4
Samuel Melo <samuel.fernandes.melo@gmail.com>
2023-04-11 17:25:07 +0100

    Improved encoding and added docker compose for memcache creation

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 import hashlib
+import os
 from flask import Flask, request, abort, jsonify
 from pymemcache.client.base import Client
 
 URL_SIZE = 7
 MEMCACHE_HOST = "localhost"
-MEMCACHE_PORT = "5001"
+MEMCACHE_PORT = "11211"
+DEBUG=os.getenv("DEBUG", False)
 
 app = Flask(__name__)
 
 class URLCache:
-    def __init__(self):
-        self.url_cache = Client(f"{MEMCACHE_HOST}:{MEMCACHE_PORT}")
+    def __init__(self, host=MEMCACHE_HOST, port=MEMCACHE_PORT):
+        self.url_cache = Client(f"{host}:{port}")
+        self.encoding = "ascii"
     
     def register(self, url):
-        shortened = hashlib.sha256(url.encode()).hexdigest()[:URL_SIZE]
-        print(url)
+        shortened = hashlib.sha1(bytes(url, encoding=self.encoding)).hexdigest()[:URL_SIZE]
         self.url_cache.add(shortened, url)
         return shortened
     
     def get(self, shortened):
-        return self.url_cache.get(shortened)
-
+        result = self.url_cache.get(shortened)
+        if not result:
+            return None
+        return str(result, encoding=self.encoding)
+    
 url_cache = URLCache()
     
 def custom_response(status_code, parameter, message):
@@ -28,7 +33,7 @@ def custom_response(status_code, parameter, message):
     response.status_code = status_code
     return response
         
-@app.route('/register/', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def register_url():
     url = request.args['url']
     if not url:
@@ -43,10 +48,7 @@ def get_full_url(url):
     full_url = str(url_cache.get(shortened=url))
     if not full_url or full_url == "None":
         return custom_response(404, "error", "url is not valid")
-    
-    # remove encoded tags
-    full_url = full_url[2:len(full_url)-1]
     return custom_response(302, "url", full_url)
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=DEBUG)
